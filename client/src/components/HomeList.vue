ea7d94a70558208aff1c2f5daebba5de3726da60
Tony-Ling <tony.ling12312@gmail.com>
2022-04-26 16:42:42 -0400

    adding homepage (WIP)

--- client/src/components/HomeList.vue
+++ client/src/components/HomeList.vue
@@ -1,7 +1,87 @@
 <template>
-    <div id="button" class="p-4">
-      <h1>Hello </h1>
+  <div id="table" class="p-4">
+    <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th style="width: 5%">KARMA</th>
+                    <th style="width: 95%">TITLE</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">0</th>
+                    <td>TITLE HERE</td>
+                </tr>
+                <tr>
+                    <th scope="row">0</th>
+                    <td>TITLE HERE</td>
+                </tr>
+                <tr>
+                    <th scope="row">0</th>
+                    <td>TITLE HERE</td>
+                </tr>
+            </tbody>
+        </table>
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#exampleModal"
+    >
+      Create Thread
+    </button>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Create Your Thread</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="title-name" class="col-form-label">Title:</label>
+                <input type="text" class="form-control" id="recipient-name" />
+              </div>
+              <div class="form-group">
+                <label for="message-text" class="col-form-label"
+                  >Message:</label
+                >
+                <textarea class="form-control" id="message-text"></textarea>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Submit Thread</button>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +91,4 @@ export default {
 </script>
 
 <style>
-
 </style>
